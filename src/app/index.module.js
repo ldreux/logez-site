89952019d143d9cb3cf57c868a272067a2e3af1b
Loïc Dreux <loic.dreux@sarbacane.com>
2016-05-11 13:59:46 +0200

    First commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('logezSite', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
